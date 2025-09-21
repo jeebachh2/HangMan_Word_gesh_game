cec8dd16889ab021cd47c12b0b90fa4d239c6b0f
jeebachh kumar <jeebachhk69@gmail.com>
2025-09-21 17:34:09 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HangMan_Word_gesh_game.git/',
+  base: '/HangMan_Word_gesh_game/'
 
 
 })
